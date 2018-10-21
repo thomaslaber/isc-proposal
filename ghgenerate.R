49d8637df0dacf53dbb8df090bb72d12b3b690ba
Thomas Laber <laber.thomas@gmail.com>
2018-10-21 17:50:02 +0200

    Updates and additions (from Christoph really)

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
-proposal<-"Boilerplate ISC Proposal"
+proposal<-"serveRless"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-"Christoph Bodner, Thomas Laber"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
