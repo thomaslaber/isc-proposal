0b501186777cadeff24d17039776bfe1df7cada3
Thomas Laber <laber.thomas@gmail.com>
2018-10-31 15:36:25 +0100

    corrections

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
 proposal<-"serveRless"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Christoph Bodner, Thomas Laber"
+author<-"Christoph Bodner, Thomas Laber, Florian Schwendinger"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
